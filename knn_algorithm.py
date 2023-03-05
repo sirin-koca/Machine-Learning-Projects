f5cd15cd896341b4a863d60ba96ae5c8534c7542
sirin <s182211@outlook.com>
2023-03-05 18:22:18 +0100

    KNN algorithm used in KNeighborsClassifier

---
+++ knn_algorithm.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+"""The Euclidean distance is a measure of the distance between two points in a multidimensional space, 
+and is commonly used in machine learning to compute the similarity between two data points."""
+
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2) ** 2))
+
+
+class KNN:
+    def __init__(self, k=3):
+        self.y_train = None
+        self.X_train = None
+        self.k = k
+
+    def fit(self, x, y):
+        self.X_train = x
+        self.y_train = y
+
+    def predict(self, x):
+        y_pred = []
+        for x in x:
+            distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+            k_indices = np.argsort(distances)[:self.k]
+            k_nearest_labels = [self.y_train[i] for i in k_indices]
+            most_common = max(set(k_nearest_labels), key=k_nearest_labels.count)
+            y_pred.append(most_common)
+        return y_pred
